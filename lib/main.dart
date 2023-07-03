65f94afbafa604f09dcf161a007bbe403adc3194
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-07-03 10:24:10 +0200

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_workout_app/blocs/cubit/workouts_cubit.dart';
+import 'package:flutter_workout_app/models/workout.dart';
+import 'package:flutter_workout_app/screens/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +13,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My Workouts",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: BlocProvider<WorkoutsCubit>(
+        create: (BuildContext context) {
+          WorkoutsCubit workoutsCubit = WorkoutsCubit();
+          if (workoutsCubit.state.isEmpty) {
+            print("Loading workouts...");
+            workoutsCubit.getWorkouts();
+          }
+          print('workouts loadded');
+          return workoutsCubit;
+        },
+        child: BlocBuilder<WorkoutsCubit, List<Workout>>(builder: (context, state) {
+          return const HomePage();
+        }),
       ),
     );
   }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_workout_app/blocs/cubit/workouts_cubit.dart';
+import 'package:flutter_workout_app/models/workout.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Workouts"),
+        actions: const [
+          IconButton(onPressed: null, icon: Icon(Icons.event_available)),
+          IconButton(onPressed: null, icon: Icon(Icons.settings)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: BlocBuilder<WorkoutsCubit, List<Workout>>(
+          builder: (context, workouts) {
+            return ExpansionPanelList.radio(
+              children: workouts
+                  .map(
+                    (workout) => ExpansionPanelRadio(
+                      value: workout,
+                      headerBuilder: (context, isExpanded) {
+                        return ListTile(
+                          visualDensity: const VisualDensity(
+                            horizontal: 0,
+                            vertical: VisualDensity.maximumDensity,
+                          ),
+                          leading: const IconButton(
+                            onPressed: null,
+                            icon: Icon(Icons.edit),
+                          ),
+                          title: Text(workout.title!),
+                        );
+                      },
+                      body: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: workout.exercises.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            onTap: null,
+                            title: Text(workout.exercises[index].title!),
+                            visualDensity: const VisualDensity(
+                              horizontal: 0,
+                              vertical: VisualDensity.maximumDensity,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                  .toList(),
+            );
+            // return Center(child: Text("No data yet!"));
+          },
+        ),
+      ),
+    );
+  }
+}
